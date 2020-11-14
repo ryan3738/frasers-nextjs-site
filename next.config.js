3a0fd76a0274879b561645eab21f64f66b130e2a
Ryan <ryan3738@gmail.com>
2020-11-13 17:56:22 -0800

    netlify ssr setup

--- next.config.js
+++ next.config.js
@@ -5,3 +5,9 @@
 //       'CLOUDINARY_URL=cloudinary://886343861584429:fPwkUZIaxFWXkHVExvDQNlYrVmc@ryan3738',
 //   },
 // };
+// next.config.js
+
+module.exports = {
+  // Target must be serverless
+  target: "serverless",
+};
