4dcc700d17c91122ab78b323fb5236045c187917
Ryan <ryan3738@gmail.com>
2020-11-25 14:55:28 -0800

    add hint to alt text and make subtitle not required

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   images: {
-//     loader: 'cloudinary',
-//     path:
-//       'CLOUDINARY_URL=cloudinary://886343861584429:fPwkUZIaxFWXkHVExvDQNlYrVmc@ryan3738',
-//   },
-// };
-// next.config.js
-
 // For netlify hosting support
 module.exports = {
   // Target must be serverless
