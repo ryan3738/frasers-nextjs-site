d6809e3bb63414248db0ceae960b315a49b1ca45
Ryan <ryan3738@gmail.com>
2021-06-03 05:54:08 -0700

    create link for site editing

---
+++ components/Dashboard.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Button from './Button'
+import Layout, { siteTitle } from './Layout'
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <Head>
+        <title>DASHBOARD | {siteTitle}</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <div className="contact-content">
+        <div id="contact-info">
+          <h2>Dashboard</h2>
+          <p>Click below to edit site details</p>
+          <Button
+            buttonText="Edit Site Data"
+            buttonLink="https://frasers-admin.netlify.app/admin/index.html"
+          />
+        </div>
+      </div>
+      <style jsx>{`
+        /* .contact-container {
+          display: flex;
+          flex-direction: column;
+        } */
+        /* .contact-header {
+          padding: 3rem 0 0;
+        } */
+
+        .contact-content {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 2rem 1rem;
+        }
+        .social-media-container {
+          display: flex;
+          flex-direction: column;
+          /* justify-content: flex-start; */
+          /* align-items: flex-start; */
+          margin: 10px;
+        }
+        .social-media-contents {
+          display: flex;
+          flex-flow: row wrap;
+          justify-content: center;
+        }
+      `}</style>
+    </Layout>
+  )
+}
