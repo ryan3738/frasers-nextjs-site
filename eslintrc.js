607a15397c26a0849c0e49866e055c255262cd2b
Ryan <ryan3738@gmail.com>
2021-10-26 15:20:03 -0700

    changing linting and prettier rules

---
+++ eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  plugins: ['simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:security/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended'
+  ],
+  rules: {
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/sort': 'off',
+    'unicorn/filename-case': 'off'
+  }
+};
