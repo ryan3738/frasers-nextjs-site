f602657c350cf58821b7ada833cdfaf90e71fd78
Ryan <ryan3738@gmail.com>
2021-10-26 17:42:39 -0700

    formatting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
