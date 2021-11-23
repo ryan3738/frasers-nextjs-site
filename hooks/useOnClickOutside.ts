6d08848272880b39fee40dd74e8f1aeadf956d6c
Ryan <ryan3738@gmail.com>
2021-11-23 12:06:48 -0800

    rename hook file for easier reference

--- hooks/useOnClickOutside.ts
+++ hooks/useOnClickOutside.ts
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 
-export const useOnClickOutside = (ref, handler) => {
+export const useOnClickOutside = (ref, handler): void => {
   useEffect(() => {
-    const listener = (event) => {
+    const listener = (event): void => {
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
