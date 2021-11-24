c224f9d462ae8d36f499a19c0b10968afc98dc26
Ryan <ryan3738@gmail.com>
2021-11-23 17:09:16 -0800

    remove console.log

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -10,7 +10,6 @@ interface MenuProps {
 export const Menu = ({categories, pathName}:MenuProps) => {
     const links = categories.map(category => {
         return { href: `${pathName}#${category}`, text: category}});
-    console.log('LINKS', links);
    return (
         <>
             {categories.map((category) => <MenuSection key={category} category={category}/>)}
