65e74968028afe7037abe09b34dda69b36cb3bb0
Ryan <ryan3738@gmail.com>
2021-12-09 12:00:09 -0800

    add newsletter component for resusability

---
+++ components/NewsLetter.tsx
@@ -0,0 +1,18 @@
+import Button from "./Button"
+
+const NewsLetter = () => {
+return (
+    <>
+        <h3>Sign up for our newsletter to receive updates</h3>
+        <Button
+        buttonText="SIGN UP"
+        buttonLink="https://squareup.com/outreach/sgPDT7/subscribe"
+        />
+        <br />
+        <br />
+        <p>We respect your privacy</p>
+    </>
+    )
+}
+
+export default NewsLetter;
