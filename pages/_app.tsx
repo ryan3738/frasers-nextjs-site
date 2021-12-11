0c737f4dd89ffa51babf6632a581321efc84d82e
Ryan <ryan3738@gmail.com>
2021-12-10 18:01:19 -0800

    bootstrap tina cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
 import '../styles/global.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }) => {
   return (
-      <Component {...pageProps} />
-  );
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            {...pageProps}
+          >
+            {(livePageProps) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  )
 }
+
+export default App
