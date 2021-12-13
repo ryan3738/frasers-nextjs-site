f11a6f9e02bf100bcad37970581fa8436fd59a4f
Ryan <ryan3738@gmail.com>
2021-12-13 12:07:36 -0800

    stop ignoring type errors on build

--- next.config.js
+++ next.config.js
@@ -4,12 +4,5 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true
   }
 };
