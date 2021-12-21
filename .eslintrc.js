c6d2f219108bd2f138414ccaa5813b526c20f852
Ryan <ryan3738@gmail.com>
2021-12-20 19:39:43 -0800

    update exlint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,5 +45,64 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  overrides: [
+    // This configuration will apply only to TypeScript files
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      settings: { react: { version: 'detect' } },
+      env: {
+        browser: true,
+        node: true,
+        es6: true
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended', // TypeScript rules
+        'plugin:react/recommended', // React rules
+        'plugin:react-hooks/recommended', // React hooks rules
+        'plugin:jsx-a11y/recommended', // Accessibility rules
+        'prettier/prettier', // Prettier plugin
+        'plugin:prettier/recommended' // Prettier recommended rules
+      ],
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            printWidth: 80,
+            trailingComma: 'es5',
+            tabWidth: 2,
+            semi: true,
+            singleQuote: true,
+            useTabs: false,
+            // below line only for windows users facing CLRF and eslint prettier error
+            // non windows users feel free to delete it
+            endOfLine: 'auto'
+          }
+        ],
+        // Includes .prettierrc.js rules
+        // We will use TypeScript's types for component props instead
+        'react/prop-types': 'off',
+
+        // No need to import React when using Next.js
+        'react/react-in-jsx-scope': 'off',
+
+        // This rule is not compatible with Next.js's <Link /> components
+        'jsx-a11y/anchor-is-valid': 'off',
+
+        // Why would you want unused vars?
+        '@typescript-eslint/no-unused-vars': ['error'],
+
+        // I suggest this setting for requiring return types on functions only where useful
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true
+          }
+        ]
+      }
+    }
+  ]
 };
