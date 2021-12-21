ac3434ddf809dfd894dca8ba3f6e9ed898593bf6
Ryan <ryan3738@gmail.com>
2021-12-20 16:16:46 -0800

    remove console.log

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -13,17 +13,14 @@ const Menu = ({ menu, sections, pathName }:MenuProps) => {
     if (!menu) {
         return <div>No Menu Found</div>
     }
-    console.log("Menu:", menu)
     const links = menu.sections.map(section => {
     return { href: `${pathName}#${slugify(section.name)}`, text: section.name}});
-    console.log('LINKS', links)
 
     if (!menu) { return <div>No Menu Found</div> }
    return (
         <>
             {sections.map((item) =>{
                 const section = menu?.sections?.find(section => section.name === item);
-                console.log('SECTION', section)
                 return<MenuSection section={section} key={item} category={item} id={slugify(section?.name)}/>})}
             <NavBar showHomeLink={false} links={links} position='sticky' location='bottom' />
         </>
