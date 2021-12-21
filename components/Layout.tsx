5715bff52742571155baada18e44f163d6aee44b
Ryan <ryan3738@gmail.com>
2021-12-20 21:19:47 -0800

    change file extension to .json

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -109,7 +109,7 @@ export default function Layout({
             sizes="16x16"
             href="/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest.json" />
         </Head>
         <header className="header">
           {home ? (
