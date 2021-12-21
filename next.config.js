519d6cc701f2ec72e7926e6954527c5c52f5062d
Ryan <ryan3738@gmail.com>
2021-12-20 21:29:31 -0800

    add pwa dep

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-// eslint-disable-next-line unicorn/prefer-module
-module.exports = {
+/* eslint-disable unicorn/prefer-module */
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true
   }
-};
+});
