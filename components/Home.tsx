42ce69e55b50bed20cbfc1a79b83288fae638f79
Ryan <ryan3738@gmail.com>
2022-02-07 13:22:01 -0600

    Remove new-years button

--- components/Home.tsx
+++ components/Home.tsx
@@ -33,16 +33,12 @@ export default function Home({ menu, businessInfo }: HomeProps): JSX.Element {
           <div className="double-wrapper">
             <Double
               id="about"
-              title="Open New Year's Eve"
-              subtitle="Special 5 Course Menu"
+              title=""
+              subtitle=""
               imageSource="/images/gift-card-square.jpg"
               imageAlt="frasers gift card closeup"
             >
               <div>
-                <ButtonLink
-                  buttonText="New Year's Menu"
-                  buttonLink="/menu/new-years"
-                />
                 <p>Please call to make a reservation</p>
                 <h3>Gift Cards Available</h3>
                 To purchase a gift card please call us at{' '}
