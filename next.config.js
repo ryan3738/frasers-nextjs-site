c56022b049dc06fc420926f0e26e2f09b5efd8f0
Ryan <ryan3738@gmail.com>
2022-02-22 15:20:13 -0800

    add language attribute

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable unicorn/prefer-module */
 
 module.exports = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
