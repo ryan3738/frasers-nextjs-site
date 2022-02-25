2430a22794da3385548cbd3493ac0bb00790f1ca
Ryan <ryan3738@gmail.com>
2022-02-25 07:45:17 -0800

    Change color selector, remove header

--- .tina/Global.ts
+++ .tina/Global.ts
@@ -1,162 +1,5 @@
 import { TinaField } from "@tinacms/cli"
 
-const iconSchema: TinaField = {
-  type: "object",
-  label: "Icon",
-  name: "icon",
-  fields: [
-    {
-      type: "string",
-      label: "Color",
-      name: "color",
-      options: [
-        {
-          label: "Primary",
-          value: "primary",
-        },
-        {
-          label: "Blue",
-          value: "blue",
-        },
-        {
-          label: "Teal",
-          value: "teal",
-        },
-        {
-          label: "Green",
-          value: "green",
-        },
-        {
-          label: "Red",
-          value: "red",
-        },
-        {
-          label: "Pink",
-          value: "pink",
-        },
-        {
-          label: "Purple",
-          value: "purple",
-        },
-        {
-          label: "Orange",
-          value: "orange",
-        },
-        {
-          label: "Yellow",
-          value: "yellow",
-        },
-      ],
-    },
-    {
-      name: "style",
-      label: "Style",
-      type: "string",
-      options: [
-        {
-          label: "Circle",
-          value: "circle",
-        },
-        {
-          label: "Float",
-          value: "float",
-        },
-      ],
-    },
-    {
-      type: "string",
-      label: "Icon",
-      name: "name",
-      options: [
-        {
-          label: "Random",
-          value: "",
-        },
-        {
-          label: "Aperture",
-          value: "aperture",
-        },
-        {
-          label: "Code Block",
-          value: "code",
-        },
-        {
-          label: "Like",
-          value: "like",
-        },
-        {
-          label: "Map",
-          value: "map",
-        },
-        {
-          label: "Palette",
-          value: "palette",
-        },
-        {
-          label: "Pie Chart",
-          value: "chart",
-        },
-        {
-          label: "Pin",
-          value: "pin",
-        },
-        {
-          label: "Shield",
-          value: "shield",
-        },
-        {
-          label: "Setting Sliders",
-          value: "settings",
-        },
-        {
-          label: "Store",
-          value: "store",
-        },
-        {
-          label: "Tennis Ball",
-          value: "ball",
-        },
-        {
-          label: "Test Tube",
-          value: "tube",
-        },
-        {
-          label: "Trophy",
-          value: "trophy",
-        },
-        {
-          label: "User",
-          value: "user",
-        },
-        {
-          label: "Beer",
-          value: "beer",
-        },
-        {
-          label: "Chat",
-          value: "chat",
-        },
-        {
-          label: "Cloud",
-          value: "cloud",
-        },
-        {
-          label: "Coffee",
-          value: "coffee",
-        },
-        {
-          label: "World",
-          value: "world",
-        },
-        {
-          label: "Tina",
-          value: "tina",
-        },
-      ],
-    },
-  ],
-};
-
 const navigationSchema: TinaField = {
   type: "object",
   label: "Navigation",
@@ -205,48 +48,6 @@ const navigationSchema: TinaField = {
       },
     },
   ]
-}
-
-const headerSchema: TinaField = {
-  type: "object",
-  label: "Header",
-  name: "header",
-  fields: [
-    iconSchema,
-    {
-      type: "string",
-      label: "Color",
-      name: "color",
-      options: [
-        { label: "Default", value: "default" },
-        { label: "Primary", value: "primary" },
-      ],
-    },
-    {
-      type: "object",
-      label: "Nav Links",
-      name: "nav",
-      list: true,
-      ui: {
-        defaultItem: {
-          href: "home",
-          label: "Home",
-        },
-      },
-      fields: [
-        {
-          type: "string",
-          label: "Link",
-          name: "href",
-        },
-        {
-          type: "string",
-          label: "Label",
-          name: "label",
-        },
-      ],
-    },
-  ],
 }
 
 const footerSchema: TinaField = {
@@ -299,77 +100,12 @@ const themeSchema: TinaField = {
   name: "theme",
   fields: [
     {
-      type: "string",
-      label: "Primary Color",
+      type: 'string',
       name: "color",
-      options: [
-        {
-          label: "Blue",
-          value: "blue",
-        },
-        {
-          label: "Teal",
-          value: "teal",
-        },
-        {
-          label: "Green",
-          value: "green",
-        },
-        {
-          label: "Red",
-          value: "red",
-        },
-        {
-          label: "Pink",
-          value: "pink",
-        },
-        {
-          label: "Purple",
-          value: "purple",
-        },
-        {
-          label: "Orange",
-          value: "orange",
-        },
-        {
-          label: "Yellow",
-          value: "yellow",
-        },
-      ],
-    },
-    {
-      type: "string",
-      name: "font",
-      label: "Font Family",
-      options: [
-        {
-          label: "System Sans",
-          value: "sans",
-        },
-        {
-          label: "Nunito",
-          value: "nunito",
-        },
-        {
-          label: "Lato",
-          value: "lato",
-        },
-      ],
-    },
-    {
-      type: "string",
-      name: "icon",
-      label: "Icon Set",
-      options: [
-        {
-          label: "Boxicons",
-          value: "boxicon",
-        },
-        {
-          label: "Heroicons",
-          value: "heroicon",
-        },
-      ],
+      label: "Primary Color",
+      ui: {
+        component: 'color',
+      }
     },
     {
       type: "string",
@@ -393,4 +129,4 @@ const themeSchema: TinaField = {
   ],
 }
 
-export { headerSchema, footerSchema, themeSchema, navigationSchema };
+export { footerSchema, themeSchema, navigationSchema };
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ import Header from './Header';
 import { BusinessInfo } from '../.tina/__generated__/types';
 import layoutData from '../content/global/index.json';
 import Footer from './Footer';
+import chroma from 'chroma-js';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -37,6 +38,7 @@ export default function Layout({
 }: LayoutProps): JSX.Element {
   const [open, setOpen] = useState(false);
   const router = useRouter();
+  const primaryColor = data?.theme?.color || 'teal';
   return (
     <>
       <Header navLinks={data.navigation} open={open} setOpen={setOpen} />
@@ -121,8 +123,8 @@ export default function Layout({
             --black-color: #0b0a0a;
             --surface-color: rgba(27, 15, 15, 0.03);
             --background-color: var(--black-color);
-            --primary-color: ${data.theme.color || `#c6a938`};
-            --primary-color-desaturated: #f3e08d;
+            --primary-color: var(--primary-color-500);
+            --primary-color-desaturated: var(--primary-color-200);
             /* Have option for internet explorer without f6*/
             /* 87% */
             --high-emphasis-text: var(--white-color);
@@ -134,6 +136,16 @@ export default function Layout({
             --color-instagram: #fb3958;
             --small-screen: ${theme.smallScreen};
             --medium-screen: ${theme.mediumScreen};
+            --primary-color-50: ${chroma(primaryColor).brighten(3).hex()};
+            --primary-color-100: ${chroma(primaryColor).brighten(2.25).hex()};
+            --primary-color-200: ${chroma(primaryColor).brighten(1.5).hex()};
+            --primary-color-300: ${chroma(primaryColor).brighten(0.75).hex()};
+            --primary-color-400: ${chroma(primaryColor).hex()};
+            --primary-color-500: ${chroma(primaryColor).darken(0.75).hex()};
+            --primary-color-600: ${chroma(primaryColor).darken(1.5).hex()};
+            --primary-color-700: ${chroma(primaryColor).darken(2.25).hex()};
+            --primary-color-800: ${chroma(primaryColor).darken(3).hex()};
+            --primary-color-900: ${chroma(primaryColor).darken(3.75).hex()};
           }
           .medium-emphasis {
             color: var(--medium-emphasis-text);
