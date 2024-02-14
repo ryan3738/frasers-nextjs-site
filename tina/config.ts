154950212c16722126ca6177327de71dd909b43a
ryan3738 <ryan3738@gmail.com>
2024-02-13 17:59:26 -0800

    move tina files

---
+++ tina/config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "tinacms";
+import { schema } from "./schema";
+
+
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  'dev'
+
+const token = process.env.NEXT_PUBLIC_TINA_GITHUB_TOKEN;
+const clientId = process.env.NEXT_PUBLIC_TINA_CLIENT_ID;
+const indexerToken = process.env.NEXT_PUBLIC_TINA_INDEXER_TOKEN;
+
+
+export default defineConfig({
+  branch,
+  token,
+  clientId,
+  build: {
+    publicFolder: "public", // The public asset folder for your framework
+    outputFolder: "admin", // within the public folder
+  },
+  schema,
+  search: {
+    tina: {
+      indexerToken,
+      stopwordLanguages: ['eng'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
+  media: {
+    // Media config
+    // tina: {
+    //   // Repo-based Media config
+    // },
+    // function that loads  the media store
+    loadCustomStore: async () => {
+      const pack = await import("next-tinacms-cloudinary");
+      return pack.TinaCloudCloudinaryMediaStore;
+    },
+  },
+});
+
+
